c0bab8c95574978862fd48332b85099f6de65fbe
JoseAzmitia <azmitiajc@gmail.com>
2021-12-01 13:57:02 -0600

    Se agregó la dependencia gson

--- src/main/java/evidencia/xyz/Main.java
+++ src/main/java/evidencia/xyz/Main.java
@@ -1,4 +1,15 @@
 package evidencia.xyz;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void menu(){
+        System.out.println("Aquí va el menú");
+    }
+
+    public static void validarCredenciales(){
+        System.out.println("Aquí se validan credenciales");
+    }
 }
